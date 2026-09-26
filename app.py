9a50b8c3c63ec9ca3c03ca08eb9b32125613f0bd
rajendrakmr <rajendraakmr@gmail.com>
2026-09-26 11:40:48 +0530

    add python lint workflow

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+
+@app.route("/hello/<name>")
+def hello(name):
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
